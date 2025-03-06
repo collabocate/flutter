a4bf3a3c244a643c5ac9a0b009da83edb96cac99
Oyebliss <oyebisi695@gmail.com>
2025-03-06 16:53:33 +0100

    Setup floating action button to open collabocate screen on click.

--- lib/collabocate.dart
+++ lib/collabocate.dart
@@ -17,8 +17,6 @@ class _CollabocateState extends State<Collabocate> {
   GitHubService? _service;
   List<IssueTemplate> templates = [];
   String? selectedTemplateType;
-  final titleController = TextEditingController();
-  final bodyController = TextEditingController();
   bool isLoading = false;
   bool isInitialized = false;
   String? errorMessage;
@@ -47,7 +45,9 @@ class _CollabocateState extends State<Collabocate> {
   Future<void> _fetchTemplates() async {
     if (_service == null) return;
 
-    setState(() => isLoading = true);
+    setState(
+      () => isLoading = true,
+    );
     try {
       templates = await _service!.fetchIssueTemplates();
     } catch (e) {
@@ -57,95 +57,6 @@ class _CollabocateState extends State<Collabocate> {
       );
     } finally {
       setState(() => isLoading = false);
-    }
-  }
-
-  Future<void> _updateIssueBody(String? templateType) async {
-    if (_service == null || templateType == null) return;
-
-    setState(() => isLoading = true);
-    try {
-      final template = templates.firstWhere(
-        (template) => template.name == templateType,
-        orElse: () => IssueTemplate(
-          name: '',
-          downloadUrl: '',
-        ),
-      );
-      final templateBody =
-          await _service!.fetchTemplateBody(template.downloadUrl);
-      setState(() {
-        selectedTemplateType = templateType;
-        bodyController.text = templateBody;
-      });
-    } catch (e) {
-      if (!mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            'Failed to load template: $e',
-          ),
-          backgroundColor: Colors.red,
-        ),
-      );
-    } finally {
-      setState(
-        () => isLoading = false,
-      );
-    }
-  }
-
-  Future<void> _createIssue() async {
-    if (_service == null) return;
-
-    final title = titleController.text.trim();
-    final body = bodyController.text.trim();
-
-    if (title.isEmpty || body.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
-            'Please provide both title and body for the issue.',
-          ),
-          backgroundColor: Colors.red,
-        ),
-      );
-      return;
-    }
-
-    setState(
-      () => isLoading = true,
-    );
-    try {
-      await _service!.createIssue(title, body);
-      if (!mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
-            'Issue created successfully!',
-          ),
-          backgroundColor: Colors.green,
-        ),
-      );
-      titleController.clear();
-      bodyController.clear();
-      setState(() {
-        selectedTemplateType = null;
-      });
-    } catch (e) {
-      if (!mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            e.toString(),
-          ),
-          backgroundColor: Colors.red,
-        ),
-      );
-    } finally {
-      setState(
-        () => isLoading = false,
-      );
     }
   }
 
@@ -175,8 +86,150 @@ class _CollabocateState extends State<Collabocate> {
     }
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Collabocate Flutter'),
+        title: Text('Collabocate'),
         centerTitle: true,
+        elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => IssueForm(
+                templates: templates,
+                service: _service,
+                onIssueCreated: _fetchTemplates,
+              ),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class IssueForm extends StatefulWidget {
+  final List<IssueTemplate> templates;
+  final GitHubService? service;
+  final VoidCallback? onIssueCreated;
+
+  const IssueForm({
+    super.key,
+    required this.templates,
+    this.service,
+    this.onIssueCreated,
+  });
+
+  @override
+  State<IssueForm> createState() => _IssueFormState();
+}
+
+class _IssueFormState extends State<IssueForm> {
+  String? selectedTemplateType;
+  final titleController = TextEditingController();
+  final bodyController = TextEditingController();
+  bool isLoading = false;
+
+  Future<void> _updateIssueBody(String? templateType) async {
+    if (widget.service == null || templateType == null) return;
+
+    setState(() => isLoading = true);
+    try {
+      final template = widget.templates.firstWhere(
+        (template) => template.name == templateType,
+        orElse: () => IssueTemplate(
+          name: '',
+          downloadUrl: '',
+        ),
+      );
+      final templateBody =
+          await widget.service!.fetchTemplateBody(template.downloadUrl);
+      setState(() {
+        selectedTemplateType = templateType;
+        bodyController.text = templateBody;
+      });
+    } catch (e) {
+      if (!mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Failed to load template: $e',
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } finally {
+      setState(
+        () => isLoading = false,
+      );
+    }
+  }
+
+  Future<void> _createIssue() async {
+    if (widget.service == null) return;
+
+    final title = titleController.text.trim();
+    final body = bodyController.text.trim();
+
+    if (title.isEmpty || body.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'Please provide both title and body for the issue.',
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+
+    setState(
+      () => isLoading = true,
+    );
+    try {
+      await widget.service!.createIssue(title, body);
+      if (!mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'Issue created successfully!',
+          ),
+          backgroundColor: Colors.green,
+        ),
+      );
+
+      if (widget.onIssueCreated != null) {
+        widget.onIssueCreated!();
+      }
+
+      Navigator.of(context).pop();
+    } catch (e) {
+      if (!mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            e.toString(),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } finally {
+      setState(
+        () => isLoading = false,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Report Issue',
+        ),
+        centerTitle: true,
+        elevation: 0,
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20),
@@ -198,19 +251,15 @@ class _CollabocateState extends State<Collabocate> {
                 ),
                 labelText: 'Template Type',
               ),
-              items: templates.map((template) {
+              items: widget.templates.map((template) {
                 return DropdownMenuItem(
                   value: template.name,
-                  child: Text(
-                    template.name,
-                  ),
+                  child: Text(template.name),
                 );
               }).toList(),
               onChanged: (value) => _updateIssueBody(value),
             ),
-            const SizedBox(
-              height: 15,
-            ),
+            const SizedBox(height: 15),
             TextFormField(
               controller: titleController,
               decoration: InputDecoration(
@@ -245,9 +294,7 @@ class _CollabocateState extends State<Collabocate> {
                   padding: const EdgeInsets.all(18.0),
                   child: isLoading
                       ? const CircularProgressIndicator()
-                      : const Text(
-                          'Create Issue',
-                        ),
+                      : const Text('Create Issue'),
                 ),
               ),
             ),
